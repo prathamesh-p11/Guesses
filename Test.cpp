e9c1e12f284e3f6264c847f25b5263962038ae71
prathamesh-p11 <prathameshp2472@gmail.com>
2020-10-13 17:09:54 -0700

    Result

---
+++ Test.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+void ReadAndPrint()
+{
+    std::ifstream file;
+    std::string word, filename;
+    filename = "words_alpha.txt";
+    file.open(filename.c_str());
+    
+    std::ofstream op;
+    op.open("Guesses.txt");
+    // extract words and insert in new file 
+    while (file >> word && word[0] <= 'e' )
+    {
+        op << word <<"\n";
+    }
+}
+int main()
+{
+	ReadAndPrint();
+	return 0;
+}
